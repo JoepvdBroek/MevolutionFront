29e78fb79d394fc46b2ecd9f02a3e091aa5c9e13
= <milan@verbaandert.com>
2015-05-09 11:50:33 +0000

    Preruben commit

--- resources/assets/js/libs.js
+++ resources/assets/js/libs.js
@@ -3,4 +3,5 @@ require('angular-route');
 require('angular-resource');
 require('angular-css');
 require('bootstrap');
+require('jquery-colorbox');
 require('../bower_components/zoomooz/jquery.zoomooz.js');
