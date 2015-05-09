ea4f4a5b2be860bda7af6a18c1355b0345ebb778
= <milan@verbaandert.com>
2015-05-09 11:28:53 +0000

    Moet

--- resources/assets/js/libs.js
+++ resources/assets/js/libs.js
@@ -2,3 +2,5 @@ require('angular');
 require('angular-route');
 require('angular-resource');
 require('angular-css');
+require('bootstrap');
+require('../bower_components/zoomooz/jquery.zoomooz.js');
