34d405c7662d1e6fbfb62c46992bc14f4fe05c0e
rubendehaas <rdehaas92@gmail.com>
2015-06-13 03:45:54 +0200

    Admin layout changes (all pages)

--- resources/assets/js/Admin/Controllers/AdminController.js
+++ resources/assets/js/Admin/Controllers/AdminController.js
@@ -2,6 +2,16 @@ module.exports = function(admin)
 {
     admin.controller('AdminController', [ '$scope', '$location', '$window', '$routeParams', '$rootScope', 'OrganisationService', 'GroupService', 'UserGroupService', 'AdminFactory', function($scope, $location, $window, $routeParams, $rootScope, OrganisationService, GroupService, UserGroupService, AdminFactory)
     {
+
+        $scope.go = function(path){
+            console.log(path);
+            $location.path(path);
+        };
+
+        $scope.goBack = function(){
+            $window.history.back();
+        };
+        
         // @TODO filter the ng-repeat on users who don't have an organisation yet???
         var history = [];
 
