dc84899df98be53c9fcd2f2118e206460c1b8760
= <milan@verbaandert.com>
2015-06-23 22:41:35 +0000

    New install script

--- install.sh
+++ install.sh
@@ -6,18 +6,15 @@ composer install
 # Install npm dependencies
 npm install
 
-# Install bower dependencies
-bower install
-
 # Write .bowerrc file
-echo <<EOT > .bowerrc
+cat > .bowerrc <<EOT
 {
     "directory" : "resources/assets/bower_components"
 }
 EOT
 
 # Write .env file
-echo <<EOT > .env
+cat > .env <<EOT
 APP_ENV=local
 APP_DEBUG=false
 APP_KEY=7m113gubxyW3mtYjhl5klhJimL8vNUzb
@@ -26,6 +23,12 @@ CACHE_DRIVER=file
 SESSION_DRIVER=file
 QUEUE_DRIVER=sync
 EOT
+
+# Install Bower components
+bower install
+
+# Set file permissions
+chmod -R 0777 storage/
 
 # Run gulp
 gulp
